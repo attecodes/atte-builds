920d2a6e0ce971684d5c7f3082c967ac9f832234
Atte Myllykoski <atte.myllykoski@gmail.com>
2022-07-20 15:30:28 -0700

    remove sanity.cli

--- cms/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {createCliConfig} from '@sanity/cli'
-
-export default createCliConfig({
-  api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_STUDIO_API_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_STUDIO_API_DATASET,
-  },
-})
